8185da04d5974ff03e37edebe7976f68f0a1a81a
Jo <Jo@qa.com>
2024-09-03 14:23:44 +0100

    first java job

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        system.out.println(sayHelloToSomeone("Jo"));
         System.out.println(sayGoodbye());
     }
 
